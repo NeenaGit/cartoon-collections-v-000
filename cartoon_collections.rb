800effddb8b2aa0edf178b9897c76118baac1dce
Neena van de Water <nkuriakose@hotmail.com>
2017-11-14 17:21:50 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,5 +24,5 @@ end
 
 def find_the_cheese(cheddar_cheese)
   # cheese_types = ["cheddar", "gouda", "camembert"]
-  cheddar_cheese.first.include? { | cheese_types | cheese_types == ["cheddar", "gouda", "camembert"]}
+  cheddar_cheese.first { | cheese_types | cheese_types == ["cheddar", "gouda", "camembert"]}
 end
